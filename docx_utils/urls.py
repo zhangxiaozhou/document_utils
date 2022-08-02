a59fc1fdae295280b3f833f904a532631cae8596
xiaozhou.zhang <xiaozhou.zhang@hexagon.com>
2022-08-02 15:07:26 +0800

    change port

--- docx_utils/urls.py
+++ docx_utils/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from  docx_utils import views
 
 urlpatterns = [
-    path('contract/', views.contract)
+    path('contract/', views.contract),
+
+    path('makeDoc/', views.makeDoc)
 ]
--- docx_utils/views.py
+++ docx_utils/views.py
@@ -18,6 +18,47 @@ def home(request):
     return render(request, 'home.html')
 
 
+def makeDoc(request):
+    
+    logger = logging.getLogger('django')
+    
+    if request.method == 'POST':
+        
+        logger.info('开始处理请求') 
+        
+        request_body = request.body 
+        
+        request_dict = json.loads(request_body) 
+        logger.info('请求参数 %s', str(request_dict))
+        
+        file_base64 = request_dict.get('file_base64')
+        file_data = base64.b64decode(file_base64)
+        template_path = save_template_file(file_data)
+         
+        contract_docx_path = render_doc(template_path, request_dict)
+        
+      
+        pdf_base64 = file2base64(contract_docx_path)
+        
+        result = {
+            'docx_base64': str(pdf_base64, encoding='utf-8'),
+            'msg': 'ok'
+        }
+        
+        logger.info('结束处理请求，并返回客户端')
+        
+        return JsonResponse(result) 
+    else:
+        result = {
+            'docx_base64': '',
+            'msg': '接口仅支持post方法,请先访问项目根路径查看文档'
+        } 
+        
+        logger.info('get请求返回错误信息给客户端')
+        
+        return JsonResponse(result)
+         
+
 def contract(request):
     
     logger = logging.getLogger('django')
